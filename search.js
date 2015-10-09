82099ea4cd6d520d1d64b90e0218645496aa1fe8
Samuel Vandamme <samuel.vandamme@coscale.com>
2015-10-09 11:30:22 +0200

    Added full text search

---
+++ search.js
@@ -0,0 +1,19 @@
+---
+---
+var docs =
+[
+    {% for post in site.docs_installation %}
+        {% include post.json %}
+    {% endfor %}
+];
+
+// init lunr
+var idx = lunr(function () {
+    this.field('title', 10);
+    this.field('content');
+});
+
+// add each document to be index
+for(var index in docs) {
+    idx.add(docs[index]);
+}
