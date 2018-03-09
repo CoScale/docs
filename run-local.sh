237fc1df7cfb2181f90bbd4cf0ec91500824eef1
Samuel Vandamme <samuel@sava.be>
2018-03-09 14:23:25 +0100

    Set UID and GID on run-local to avoid permission errors

--- run-local.sh
+++ run-local.sh
@@ -4,6 +4,8 @@ export JEKYLL_VERSION=3.5
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
   --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+  -e JEKYLL_UID=$(id -u) \
+  -e JEKYLL_GID=$(id -g) \
   -p 4000:4000 \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve --watch --incremental
